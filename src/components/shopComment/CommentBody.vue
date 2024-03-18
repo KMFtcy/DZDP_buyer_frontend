5f36ee0183c112cf05bcd01879d9542b8a3c2690
kmftang <kmftangchaoyang@gmail.com>
2024-03-18 12:29:55 +0000

    finish comment body style

--- src/components/shopComment/CommentBody.vue
+++ src/components/shopComment/CommentBody.vue
@@ -3,64 +3,84 @@
     <div class="item-intro-show">
       <div class="item-intro-detail" ref="itemIntroDetail">
         <div class="item-intro-nav item-tabs">
-              <div class="remarks-container" ref="itemGoodsComment">
-                <!-- <div class="remarks-analyse-box">
-                  <div class="remarks-analyse-goods">
-                    <i-circle :percent="skuDetail.grade" stroke-color="#5cb85c">
-                      <span class="remarks-analyse-num">{{skuDetail.grade}}%</span>
-                      <p class="remarks-analyse-title">好评率</p>
-                    </i-circle>
-                  </div>
-                </div> -->
-                <div class="remarks-bar">
-                  <span @click="viewByGrade('')" :class="{selectedBar: commentParams.grade === ''}">全部</span>
-                  <!-- <span @click="viewByGrade('')" :class="{selectedBar: commentParams.grade === ''}">全部({{commentTypeNum.all}})</span>
-                  <span @click="viewByGrade('GOOD')" :class="{selectedBar: commentParams.grade === 'GOOD'}">好评({{commentTypeNum.good}})</span>
-                  <span @click="viewByGrade('MODERATE')" :class="{selectedBar: commentParams.grade === 'MODERATE'}">中评({{commentTypeNum.moderate}})</span>
-                  <span @click="viewByGrade('WORSE')" :class="{selectedBar: commentParams.grade === 'WORSE'}">差评({{commentTypeNum.worse}})</span> -->
+          <div class="remarks-container" ref="itemGoodsComment">
+            <div class="remarks-bar"> </div>
+            <div class="review-body">
+              <div class="title">我要评价</div>
+              <div class="metric-star metric-star-wrapper">
+                <div class="metric-star-item">
+                  <div class="rc-rate-name">总体评价<em class="rc-rate-required">*</em></div>
+                  <span>
+                    <Rate v-model="overallRate" allow-half @on-change="onChange"/>
+                  </span>
+                  <div class="rc-rate-desc ">点击星星给商户打分</div>
                 </div>
-                <div style="text-align: center;margin-top: 20px;" v-if="commentList.length === 0">
-                  暂无评价数据
+                <div class="metric-star-item">
+                  <div class="rc-rate-name">口味<em class="rc-rate-required">*</em></div>
+                  <span>
+                    <Rate v-model="overallRate" allow-half @on-change="onChange"/>
+                  </span>
+                  <div class="rc-rate-desc ">给口味打分</div>
                 </div>
-                <div class="remarks-box" v-for="(item,index) in commentList" :key="index" v-else>
-                  <div class="remarks-user">
-                    <Avatar :src="item.memberProfile" />
-                    <span class="remarks-user-name">{{item.memberName | secrecyMobile}}</span>
-                  </div>
-                  <div class="remarks-content-box">
-                    <p>
-                      <Rate disabled :value="Number(item.starNum)" allow-half class="remarks-star"></Rate>
-                    </p>
-                    <p class="remarks-content">{{item.comment}}</p>
-                    <div class="comment-img" v-if="item.images">
-                      <div v-for="(img, imgIndex) in item.images"
-                       @click="previewImg(img, item)"
-                       :class="{borderColor:img === item.previewImg}"
-                       :key="imgIndex">
-                        <img :src="img" alt="">
-                      </div>
-                    </div>
-                    <div class="preview-img"  v-if="item.previewimg"  @click.prevent="hidepreviewimg(item)">
-                      <div>
-                        <span @click.stop="rotatePreviewImg(0, item)"><Icon type="md-refresh" />左转</span>
-                        <span @click.stop="rotatePreviewImg(1, item)"><Icon type="md-refresh" />右转</span>
-                      </div>
-                      <img :src="item.previewImg" :style="{transform:`rotate(${item.deg}deg)`}" width="198" alt="">
-                    </div>
-                    <p class="remarks-sub">
-                      <span class="remarks-item">{{item.storeName}}</span>
-                      <span class="remarks-time">{{item.createdTime}}</span>
-                    </p>
-                  </div>
+                <div class="metric-star-item">
+                  <div class="rc-rate-name">环境<em class="rc-rate-required">*</em></div>
+                  <span>
+                    <Rate v-model="overallRate" allow-half @on-change="onChange"/>
+                  </span>
+                  <div class="rc-rate-desc ">给环境打分</div>
                 </div>
-                <div class="remarks-page">
-                  <Page :total="commentTotal" size="small"
-                    @on-change="changePageNum"
-                    @on-page-size-change="changePageSize"
-                    :page-size="commentParams.pageSize"
-                    ></Page>
+                <div class="metric-star-item">
+                  <div class="rc-rate-name">服务<em class="rc-rate-required">*</em></div>
+                  <span>
+                    <Rate v-model="overallRate" allow-half @on-change="onChange"/>
+                  </span>
+                  <div class="rc-rate-desc ">给服务打分</div>
                 </div>
               </div>
+              <div class="text-area-module">
+                <div class="module-title-text">评价<em class="required">*</em></div>
+                <div>
+                  <div class="comment-body"><textarea class="comment-text-area" placeholder="亲，菜品的口味如何，环境怎么样，服务满意吗？"
+                      style="overflow: hidden; overflow-wrap: break-word; resize: horizontal; height: 190px;"></textarea>
+                  </div>
+                </div>
+              </div>
+              <!-- <div class="upload-image-module">
+                <div class="module-title">图片/视频</div>
+                <div class="upload-wrapper">
+                  <div class="upload-info">
+                    <div class="upload-module"><input type="file" multiple="" name="上传图片" accept="image/*" value=""
+                        class="upload-image"><button
+                        class="btn btn-1 btn-upload btn-image-upload"><span>上传图片</span></button>
+                    </div>
+                    <div class="upload-desc">单张图片不超过10MB，尺寸不小于300*300px，支持jpg、png和bmp格式</div>
+                  </div>
+                  <div class="pic-wrapper"></div><button class="btn btn-1 btn-upload"><span>上传视频</span></button>
+                </div>
+              </div> -->
+              <!-- <div class="expense-module">
+                <div class="expense-item">
+                  <div class="module-title">人均</div>
+                  <div class="expense-wrapper"><input class="expense-input" value="" data-title="人均" data-index="0">
+                    <div class="unit">元</div>
+                  </div>
+                </div>
+              </div> -->
+              <div class="warn">
+                评价和打分都将是其他网友的参考依据，并影响该商户评价。请发布真实、客观的本人消费体验评价。如您收到威
+                或并非本人体验的虚假/恶意评价，则评价视为违规，同时影响您的信誉度
+              </div>
+              <div class="btn-block-module">
+                <div class="btn-block-wrapper">
+                  <div class="btn-wrapper">
+                  <!-- <button class="btn btn-1 btn-save"><span>保存草稿</span></button> -->
+                  <button class="btn btn-2"><span>发表评价</span></button></div>
+                  <!-- <div class="save-auto-hide">评价已经在 自动保存</div> -->
+                </div>
+                <!-- <div class="save-success-hide">保存成功，此草稿仅可在PC端使用</div> -->
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -87,7 +107,7 @@ export default {
       immediate: true,
     },
   },
-  data () {
+  data() {
     return {
       commentList: [], // 评论列表
       commentParams: { // 评论传参
@@ -98,18 +118,20 @@ export default {
       },
       commentTypeNum: {}, // 评论数量，包括好中差分别的数量
       commentTotal: 0, // 评论总数
-      onceFlag: true // 只调用一次
+      onceFlag: true, // 只调用一次
+      overallRate: 0,
+      tips: ['不满意', '糟糕', '一般', '优秀', '非常满意'],
     };
   },
   computed: {
     // 商品详情
-    shopDetail () {
+    shopDetail() {
       return this.detail;
     }
   },
   methods: {
 
-    getCommentsList () { // 获取评论列表
+    getCommentsList() { // 获取评论列表
       this.commentParams.storeId = this.shopDetail.id;
       getShopsComments(this.commentParams).then(res => {
         if (res.success) {
@@ -119,32 +141,32 @@ export default {
           this.$Message.error(res.message);
           this.isLoading = false
         }
-        })
+      })
         .catch((e) => {
           console.log(e)
           this.isLoading = false
-      });
+        });
       // goodsCommentNum(this.skuDetail.goodsId).then(res => {
       //   if (res.success) {
       //     this.commentTypeNum = res.result;
       //   }
       // });
     },
-    changePageNum (val) { // 修改评论页码
+    changePageNum(val) { // 修改评论页码
       this.commentParams.pageNumber = val;
       this.getCommentsList();
     },
-    changePageSize (val) { // 修改评论页数
+    changePageSize(val) { // 修改评论页数
       this.commentParams.pageNumber = 1;
       this.commentParams.pageSize = val;
       this.getCommentsList();
     },
-    changeHeight (name) { // 设置商品详情高度
+    changeHeight(name) { // 设置商品详情高度
       let heightCss = window.getComputedStyle(this.$refs[name]).height;
       heightCss = parseInt(heightCss.substr(0, heightCss.length - 2)) + 89;
       this.$refs.itemIntroDetail.style.height = heightCss + 'px';
     },
-    handleScroll () { // 监听页面滚动
+    handleScroll() { // 监听页面滚动
       if (this.onceFlag) {
         this.$nextTick(() => {
           this.changeHeight('itemGoodsComment')
@@ -152,17 +174,20 @@ export default {
         this.onceFlag = false
       }
     },
-    previewImg (img, item) { // 预览图片
+    previewImg(img, item) { // 预览图片
       this.$set(item, 'previewImg', img);
       this.$nextTick(() => {
         this.changeHeight('itemGoodsComment')
       });
     },
-    updateCommentsHeight(){
+    updateCommentsHeight() {
       this.changeHeight('itemGoodsComment')
-    }
+    },
+    onChange(value) {
+      console.log('你打了'+value+'分');
+    },
   },
-  mounted () {
+  mounted() {
     window.addEventListener('scroll', this.handleScroll)
     this.getCommentsList();
     setTimeout(this.updateCommentsHeight, 500);
@@ -170,296 +195,222 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-.item-intro{
-  >img{
-    display:block;
-  }
-}
-/***************商品详情介绍和推荐侧边栏开始***************/
-.item-intro-show{
-
+.item-intro-show {
   width: 1200px;
   margin: 15px auto;
   display: flex;
   overflow-x: hidden;
   flex-direction: row;
+}
 
-}
-.item-intro-recommend{
-  width: 200px;
-  display: flex;
-  flex-direction: column;
-}
-.item-intro-recommend-column{
-  display: flex;
-  flex-direction: column;
-  box-shadow: 0px 0px 5px #999;
-}
-.item-recommend-title{
+.item-intro-detail {
+  margin: 0 30px;
   width: 100%;
-  height: 38px;
-  font-size: 16px;
-  line-height: 38px;
-  color: #fff;
-  background-color: $theme_color;
-  box-shadow: 0px 0px 5px $theme_color;
-  text-align: center;
+  border: 1px solid #f0f0f0;
 }
-.item-recommend-column{
-  margin-top: 15px;
-}
-.item-recommend-intro{
-  padding: 5px 15px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  font-size: 12px;
-  color: #999;
-  cursor: pointer;
-}
-.item-recommend-img{
-  width: 80%;
-  margin: 0px auto;
-  cursor: pointer;
-}
-.item-recommend-img img{
-  width: 100%;
-}
-.item-recommend-top-num{
-  color: #fff;
-  margin: 0px 2px;
-  padding: 1px 5px;
-  border-radius: 12px;
-  background-color: $theme_color;
-}
-.item-recommend-price{
-  color: $theme_color;
-  font-weight: bolder;
-}
-.item-intro-detail{
-  margin: 0  30px;
-  width: 100%;
-}
-.item-intro-nav{
-  width: 100%;
-  height: 38px;
-  background-color: #F7F7F7;
-}
-.item-intro-nav ul{
-  margin: 0px;
-  padding: 0px;
-  list-style: none;
-}
-.item-intro-nav li{
-  float: left;
-  height: 100%;
-  width: 120px;
-  line-height: 38px;
-  text-align: center;
-  color: $theme_color;
-}
-.item-intro-nav li:first-child{
-  background-color: $theme_color;
-  color: #fff;
-}
-.item-intro-img {
-  width: 100%;
-  min-height: 300px;
-  /deep/ img{
-    margin:0 auto;
-  }
-}
-.item-intro-img img{
-  max-width: 1000px;
-}
-/************* 商品参数 *************/
-.item-param-container {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.item-param-box {
-  padding: 5px;
-  padding-left: 30px;
-  width: 240px;
+
+.remarks-bar {
   height: 36px;
-  font-size: 14px;
-}
-.item-param-title {
-  color: #232323;
-}
-.item-param-content {
-  color: #999;
-}
-.remarks-title {
-  padding-left: 15px;
-  height: 36px;
-  font-size: 16px;
-  font-weight: bolder;
   line-height: 36px;
   color: #666666;
   background-color: #F7F7F7;
+
+  .selectedBar {
+    color: $theme_color;
+  }
 }
-.remarks-analyse-box {
-  padding: 15px;
+
+.review-body {
+  box-sizing: border-box;
+  padding: 24px 60px 46px 20px;
+  background: #fff;
+}
+
+.title {
+  --rem: 16;
+  -webkit-font-smoothing: antialiased;
+  user-select: none;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+  box-sizing: border-box;
+  font-family: PingFangSC-Semibold;
+  font-size: 16px;
+  color: #333;
+  margin-bottom: 17px;
+}
+
+#__dx_app__>div>div.review-module>div.review-left>div.review-body>div.metric-star.metric-star-wrapper {
+  --rem: 16;
+  -webkit-font-smoothing: antialiased;
+  user-select: none;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+  box-sizing: border-box;
+  background-color: #fff;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+}
+
+.metric-star-item {
+  --rem: 16;
+  -webkit-font-smoothing: antialiased;
+  user-select: none;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+  box-sizing: border-box;
   display: flex;
+  -webkit-box-align: center;
   align-items: center;
+  margin-bottom: 16px;
 }
-.remarks-analyse-goods {
-  margin-left: 15px;
-  margin-right: 15px;
+
+.rc-rate-name,
+.module-title-text {
+  --rem: 16;
+  -webkit-font-smoothing: antialiased;
+  user-select: none;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+  box-sizing: border-box;
+  width: 88px;
+  height: 24px;
+  line-height: 24px;
+  text-align: right;
+  color: #333;
+  margin-right: 12px;
 }
-.remarks-analyse-num {
-  font-size: 26px;
-}
-.remarks-analyse-title {
-  font-size: 12px;
+
+.rc-rate-required,
+.required {
+  --rem: 16;
+  -webkit-font-smoothing: antialiased;
+  user-select: none;
+  font-family: PingFangSC-Regular;
+  text-align: right;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+  font-style: normal;
+  color: #f63;
+  font-size: 14px;
   line-height: 20px;
 }
-.remarks-bar {
-  padding-left: 15px;
-  height: 36px;
-  line-height: 36px;
-  color: #666666;
-  background-color: #F7F7F7;
-  .selectedBar{
-    color: $theme_color;
-  }
-}
-.remarks-bar span {
-  margin-right: 15px;
-  &:hover{
-    color: $theme_color;
-    cursor: pointer;
-  }
-}
-.remarks-box {
-  padding: 15px;
+
+.text-area-module {
   display: flex;
-  flex-direction: row;
-  border-bottom: 1px #ccc dotted;
-}
-.remarks-user {
-  width: 180px;
-}
-.remarks-user-name {
-  padding-left: 15px;
-}
-.remarks-content-box {
-  width: calc(100% - 180px);
-  .comment-img{
-    display: flex;
-    .borderColor{
-      border-color: $theme_color;
-    }
-    div{
-      border: 1px solid #999;
-      margin-right: 5px;
-      width: 50px;
-      height: 50px;
-      overflow: hidden;
-      img{width: 100%;height: 100%;}
-    }
-  }
-  .preview-img{
-    position: relative;
-    border: 1px solid #eee;
-    margin: 10px 0;
-    width: 200px;
-
-    div{
-      position: absolute;
-      top: 3px;
-      left: 3px;
-      z-index: 3;
-      span{
-        display: inline-block;
-        background-color: rgba(0,0,0,.5);
-        padding:3px 5px;
-        color: #fff;
-        border-radius: 4px;
-        cursor: pointer;
-      }
-      span:nth-child(1) .ivu-icon {
-        transform: rotateY(180deg);
-      }
-    }
-
-  }
+  margin-bottom: 13px;
+  margin-top: -3px;
 }
 
-.remarks-content {
-  font-size: 14px;
-  color: #232323;
-  line-height: 28px;
-}
-.remarks-sub {
-  margin-top: 5px;
-  color: #ccc;
-}
-.remarks-time {
-  margin-left: 15px;
-}
-.remarks-page {
-  margin: 15px;
-  display: flex;
-  justify-content:flex-end;
-}
-/***************商品详情介绍和推荐侧边栏结束***************/
-/* 改变便签页样式 */
-.ivu-tabs-ink-bar {
-  background-color: $theme_color !important;
-}
-/deep/.ivu-tabs-bar{
-  border: none;
-}
-.item-tabs > .ivu-tabs > .ivu-tabs-bar .ivu-tabs-tab{
-  border-radius: 0px;
-  color: #999;
-  height: 38px;
-  // background: #F7F7F7;
-}
-.item-tabs > .ivu-tabs > .ivu-tabs-bar .ivu-tabs-tab-active{
-  color: #fff;
-  background-color: $theme_color;
-}
-.item-tabs > .ivu-tabs > .ivu-tabs-bar .ivu-tabs-tab-active:before{
-  content: '';
-  display: block;
-  width: 100%;
-  height: 1px;
-  color: #fff;
-  background: #F7F7F7;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-.ivu-rate-star-full:before, .ivu-rate-star-half .ivu-rate-star-content:before {
-  color: $theme_color;
-}
-table{
-  border-color:#efefef;
-  color: #999;
-  min-width: 30%;
-  margin-left: 30px;
+.metric-star .rc-rate-desc {
   font-size: 12px;
-  tr{
-    td:nth-child(1){
-      width: 100px;
-    }
-    td:nth-child(2){
-      padding-left: 20px;
-    }
-  }
-  td{
-    padding: 6px;
-  }
+  color: #999;
 }
-.goods-params {
-  display: flex;
-  border-bottom: 1px solid #eee;
-  margin-left: 30px;
-  span{color:#999}
+
+.warn {
+      --rem: 16;
+    -webkit-font-smoothing: antialiased;
+    user-select: none;
+    margin: 0;
+    border: 0;
+    vertical-align: baseline;
+    box-sizing: border-box;
+    width: 690px;
+    height: 54px;
+    padding: 10px;
+    background: #f6f6f6;
+    font-family: PingFangSC-Regular;
+    font-size: 12px;
+    line-height: 17px;
+    color: #999;
+    margin-left: 100px;
+    margin-bottom: 16px;
+}
+
+.btn-block-wrapper {
+    -webkit-box-pack: justify;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    margin-left: 100px;
+}
+
+.btn-wrapper {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+}
+
+.btn{
+    --rem: 16;
+    -webkit-font-smoothing: antialiased;
+    user-select: none;
+    background-color: transparent;
+    -webkit-tap-highlight-color: rgba(255,255,255,0);
+    width: 88px;
+    height: 34px;
+    cursor: pointer;
+    border-radius: 2px;
+    font-family: PingFangSC-Regular;
+    font-size: 16px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    outline: 0;
+    margin: 0;
+    padding: 0;
+    border: 1px solid #f63;
+    color: #f63;
+}
+
+.btn-save{
+    margin-right: 10px;
+}
+
+.btn-2 {
+    background: #f63;
+    color: #fff;
+}
+.comment-body {
+    width: 690px;
+    position: relative;
+    border: 1px solid #ccc;
+}
+.comment-body .comment-text-area {
+    width: 100%;
+    height: 190px;
+    background-color: #fff;
+    padding: 5px 10px 34px;
+    font-family: PingFangSC;
+    font-size: 14px;
+    line-height: 20px;
+    color: #333;
+    border: none;
+    outline: none;
+}
+.comment-body .emoji-wrap {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 29px;
+    background: #f6f6f6;
 }
 </style>
